ee4148f06bf2f2a732f0bc0c9f650fd3da13cf22
Karivool <evelynlee34@gmail.com>
2016-08-08 17:33:05 -0400

    Update components to take in profile page

--- frontend/okpizza.jsx
+++ frontend/okpizza.jsx
@@ -9,8 +9,10 @@ const hashHistory = ReactRouter.hashHistory;
 
 const App = require('./components/app.jsx');
 const LoginForm = require('./components/login.jsx');
+const SignUpForm = require('./components/signup.jsx');
 const UsersIndex = require('./components/usersindex.jsx');
 const UsersShow = require('./components/usersshow.jsx');
+const Profile = require('./components/profile.jsx');
 const SessionStore = require('./stores/session_store.js');
 const SessionActions = require('./actions/session_actions.js');
 
@@ -19,9 +21,10 @@ const routes = (
     <Route path="/" component={App}>
       <IndexRoute component={App} />
       <Route path="/login" component={ LoginForm } />
-      <Route path="/signup" component={ LoginForm } />
-      <Route path="/index" component={ UsersIndex } onEnter = { _ensureLoggedIn }/>
-      <Route path="/profile/:username" component={ UsersShow } onEnter = { _ensureLoggedIn }/>
+      <Route path="/signup" component={ SignUpForm } />
+      <Route path="/index" component={ UsersIndex } onEnter={ _ensureLoggedIn }/>
+      <Route path="/profile" component={ Profile } onEnter={ _ensureLoggedIn }/>
+      <Route path="/profile/:username" component={ UsersShow } onEnter={ _ensureLoggedIn }/>
     </Route>
   </Router>
 );

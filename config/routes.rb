0b2ea99992b90ebb80bdb8848bb4e360873bd70e
Karivool <evelynlee34@gmail.com>
2016-08-10 12:43:53 -0400

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :user, only: [:show, :index]
     resources :users, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy, :show]
+    resource :question, only: [:show, :index]
+    resources :questions, only: [:create, :show, :index]
+    resource :response, only: [:show, :index]
+    resources :responses, only: [:create, :show, :index]
   end
 
   root to: "static_pages#root"

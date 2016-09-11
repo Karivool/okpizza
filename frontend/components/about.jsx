d1ee0955d4f28fd6c62e6b58a7b039f2dae95925
Karivool <evelynlee34@gmail.com>
2016-09-10 22:15:05 -0400

    Refactor about fields

--- frontend/components/about.jsx
+++ frontend/components/about.jsx
@@ -23,32 +23,30 @@ const AboutProfile = React.createClass({
   //   }
   // },
 
-  // _onChange() {
-  //   this.forceUpdate();
-  // },
-
   render: function () {
     const viewedUser = this.props.viewedUser;
-
+    const editIcon = "https://s3.amazonaws.com/okpizza-dev/neonpizza.png";
+    const isCurrentUser = viewedUser.id === currentUser.id;
+    const aboutEl = [
+      [viewedUser.summary, "My self-summary"],
+      [viewedUser.doing, "What I am doing with my time"],
+      [viewedUser.favorites, "Favorite cooks, movies, shows, music, pizza"],
+      [viewedUser.sixthings, "The six things I could do without"],
+      [viewedUser.thinking, "I spend a lot of time thinking about"],
+      [viewedUser.typical, "On a typical night I am"],
+      [viewedUser.messageif, "You should message me if"]
+    ];
     return (
       <div className="questions-index">
         <div className="profile-body-section">
-          <p className="body-info-label">My self-summary</p>
-          <p className="body-info-text">{ viewedUser.summary }</p>
-          <p className="body-info-label">What I am doing with my time</p>
-          <p className="body-info-text">{ viewedUser.doing }</p>
-          <p className="body-info-label">Favorite cooks, movies, shows, music, and pizza</p>
-          <p className="body-info-text">{ viewedUser.favorites }</p>
-          <p className="body-info-label">The six things I could do without</p>
-          <p className="body-info-text">{ viewedUser.sixthings }</p>
-          <p className="body-info-label">I spend a lot of life thinking about</p>
-          <p className="body-info-text">{ viewedUser.thinking }</p>
-          <p className="body-info-label">On a typical night I am</p>
-          <p className="body-info-text">{ viewedUser.typical }</p>
-          <p className="body-info-label">You should message me if</p>
-          <p className="body-info-text">{ viewedUser.messageif }</p>
-
-
+            { aboutEl.map(function (mapItem, idx) {
+              if (isCurrentUser === true) {
+                  return [<p key={idx} className="body-info-label">{mapItem[1]} <img className="edit-icon" src={editIcon}></img></p>, <p key={idx + 1} className="body-info-text">{mapItem[0]}</p>];
+                } else {
+                  return [<p key={idx} className="body-info-label">{mapItem[1]}</p>, <p key={idx + 1} className="body-info-text">{mapItem[0]}</p>];
+                }
+              })
+            }
         </div>
       </div>
     );

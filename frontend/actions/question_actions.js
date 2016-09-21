bf806657b4d9cf834c82aa0a4f64f9f9f548b5fd
Karivool <evelynlee34@gmail.com>
2016-09-20 23:20:21 -0400

    Refactor actions

--- frontend/actions/question_actions.js
+++ frontend/actions/question_actions.js
@@ -1,71 +1,96 @@
 const ApiUtil = require('../util/question_util.js');
 const Dispatcher = require('../dispatcher/dispatcher.js');
-const QuestionConstants = require('../constants/question_constants.js');
+
+import {
+  QUESTION_TAKEN_IN,
+  ALL_QUESTIONS_TAKEN_IN,
+  ANSWERED_QUESTIONS_TAKEN_IN,
+  UNANSWERED_QUESTIONS_TAKEN_IN,
+  RESPONSE_TAKEN_IN
+} from '../constants/question_constants.js';
+
 const SessionStore = require('../stores/session_store.js');
 const ErrorActions = require('./error_actions.js');
 
-module.exports = {
-  fetchAllQuestions () {
-    ApiUtil.fetchAllQuestions(
-      this.takeInAllQuestions,
-      ErrorActions.setErrors);
-  },
+// Action Creators
 
-  fetchSingleQuestion (id) {
-    ApiUtil.fetchSingleQuestion(id, this.takeInSingleQuestion);
-  },
+export function fetchAnsweredQuestions(userId) {
+  ApiUtil.fetchQuestionsByResponse(
+    userId,
+    takeInAnsweredQuestions
+  );
+}
 
-  fetchAnsweredQuestions () {
-    let currentUser = SessionStore.currentUser();
-    ApiUtil.fetchQuestionsByResponse (currentUser.id,
-    this.takeInAnsweredQuestions);
-  },
+export function fetchUnansweredQuestion(userId) {
+  ApiUtil.fetchQuestionsByNoResponse(
+    userId,
+    receiveUnansweredQuestion
+  );
+}
 
-  fetchUnansweredQuestions () {
-    let currentUser = SessionStore.currentUser();
-    ApiUtil.fetchQuestionsByNoResponse (currentUser.id,
-    this.takeInUnansweredQuestions);
-  },
+export function fetchAllQuestions () {
+  ApiUtil.fetchAllQuestions(
+    takeInAllQuestions,
+    ErrorActions.setErrors);
+}
 
-  submitAnswer(answer, questionId) {
-    let currentUser = SessionStore.currentUser();
-    const response = parseInt(answer) + 1;
-    ApiUtil.createResponse(response, questionId, currentUser.id, this.takeInResponse);
-  },
+export function fetchSingleQuestion (id) {
+  ApiUtil.fetchSingleQuestion(id, takeInSingleQuestion);
+}
 
-  takeInResponse (response) {
-    Dispatcher.dispatch({
-      actionType: QuestionConstants.RESPONSE_TAKEN_IN,
-      response: response
-    });
-  },
+export function fetchUnansweredQuestions() {
+  let currentUser = SessionStore.currentUser();
+  ApiUtil.fetchQuestionsByNoResponse (currentUser.id,
+  takeInUnansweredQuestions);
+}
 
-  takeInAnsweredQuestions (questions) {
-    Dispatcher.dispatch({
-      actionType:
-      QuestionConstants.ANSWERED_QUESTIONS_TAKEN_IN,
-      questions: questions
-    });
-  },
+export function submitAnswer(answer, questionId) {
+  let currentUser = SessionStore.currentUser();
+  const response = parseInt(answer) + 1;
+  ApiUtil.createResponse(response, questionId, currentUser.id, takeInResponse);
+}
 
-  takeInUnansweredQuestions (questions) {
-    Dispatcher.dispatch({
-      actionType: QuestionConstants.UNANSWERED_QUESTIONS_TAKEN_IN,
-      questions: questions
-    });
-  },
 
-  takeInAllQuestions (questions) {
-    Dispatcher.dispatch({
-      actionType: QuestionConstants.ALL_QUESTIONS_TAKEN_IN,
-      questions: questions
-    });
-  },
+// Actions
 
-  takeInSingleQuestion (question) {
-    AppDispatcher.dispatch({
-      actionType: QuestionConstants.QUESTION_TAKEN_IN,
-      question: question
-    });
-  },
-};
+export function takeInResponse (response) {
+  Dispatcher.dispatch({
+    actionType: RESPONSE_TAKEN_IN,
+    response: response
+  });
+}
+
+export function receiveUnansweredQuestion(response) {
+  Dispatcher.dispatch({
+    actionType: RECEIVE_UNANSWERED_QUESTION,
+    response: response
+  });
+}
+
+export function takeInAnsweredQuestions(questions) {
+  Dispatcher.dispatch({
+    actionType: ANSWERED_QUESTIONS_TAKEN_IN,
+    questions: questions
+  });
+}
+
+export function takeInUnansweredQuestions (questions) {
+  Dispatcher.dispatch({
+    actionType: UNANSWERED_QUESTIONS_TAKEN_IN,
+    questions: questions
+  });
+}
+
+export function takeInAllQuestions (questions) {
+  Dispatcher.dispatch({
+    actionType: ALL_QUESTIONS_TAKEN_IN,
+    questions: questions
+  });
+}
+
+export function takeInSingleQuestion (question) {
+  Dispatcher.dispatch({
+    actionType: QUESTION_TAKEN_IN,
+    question: question
+  });
+}

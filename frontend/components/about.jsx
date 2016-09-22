ed5b6a4952c9bf1299bbe9f768379c57b400e4e8
Karivool <evelynlee34@gmail.com>
2016-09-22 09:47:48 -0400

    Begin userinfo for about

--- frontend/components/about.jsx
+++ frontend/components/about.jsx
@@ -3,6 +3,8 @@ const Link = require('react-router').Link;
 const Navbar = require('./navbar.jsx');
 const QuestionStore = require('../stores/question_store.js');
 const QuestionActions = require('../actions/question_actions.js');
+const InfoStore = require('../stores/info_store.js');
+const InfoActions = require('../actions/info_actions.js');
 
 import { currentUser } from '../stores/session_store';
 
@@ -10,15 +12,34 @@ const AboutProfile = React.createClass({
   getInitialState () {
     return {
       viewedUser: this.props.viewedUser,
-      userInfo: {}
+      userInfo: {
+        // summary: {},
+        // doing: {},
+        // favorite: {},
+        // sixthings: {},
+        // thinking: {},
+        // typical: {},
+        // messageif: {}
+      }
     };
   },
 
-  // componentDidMount () {
-  // },
+  componentDidMount () {
+    this.userInfoListener = InfoStore.addListener(this.getUserInfo);
+    console.log("component did mount");
+  },
 
-  // componentWillUnmount () {
-  // },
+  getUserInfo() {
+    debugger
+    this.setState({
+      userInfo: InfoActions.fetchUserInfo()
+    });
+    console.log("in get User Info");
+  },
+
+  componentWillUnmount () {
+    this.userInfoListener.remove();
+  },
 
   // componentWillReceiveProps (newProps) {
   //   if (newProps.params){
@@ -45,12 +66,14 @@ const AboutProfile = React.createClass({
 
   render: function () {
     const viewedUser = this.props.viewedUser;
+    const userInfo = this.state.userInfo;
     const editIcon = "https://s3.amazonaws.com/okpizza-dev/neonpizza.png";
     const isCurrentUser = viewedUser.id === currentUser().id;
+
     const aboutEl = [
       [userInfo.summary, "My self-summary", this.infoForm()],
       [userInfo.doing, "What I am doing with my time", this.infoForm()],
-      [userInfo.favorites, "Favorite cooks, movies, shows, music, pizza", this.infoForm()],
+      [userInfo.favorite, "Favorite cooks, movies, shows, music, pizza", this.infoForm()],
       [userInfo.sixthings, "The six things I could do without", this.infoForm()],
       [userInfo.thinking, "I spend a lot of time thinking about", this.infoForm()],
       [userInfo.typical, "On a typical night I am", this.infoForm()],

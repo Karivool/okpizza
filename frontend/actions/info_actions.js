93205539ffb5007bbc85ef9e82b6de205ae3a1ad
Karivool <evelynlee34@gmail.com>
2016-10-03 10:50:03 -0400

    Add constants to InfoActions

--- frontend/actions/info_actions.js
+++ frontend/actions/info_actions.js
@@ -1,7 +1,7 @@
 const ApiUtil = require('../util/info_util.js');
 const Dispatcher = require('../dispatcher/dispatcher.js');
 
-import { USER_INFO_TAKEN_IN } from '../constants/info_constants.js';
+import { USER_INFO_TAKEN_IN, USER_INFO_UPDATED } from '../constants/info_constants.js';
 
 const SessionStore = require('../stores/session_store.js');
 const ErrorActions = require('./error_actions.js');

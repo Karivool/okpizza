df69a4bcd1e9f0190480f5494c1c4bd18779536f
Karivool <evelynlee34@gmail.com>
2016-10-08 18:45:45 -0400

    Create skeleton for messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def message_params
+    params.require(:messages).permit(:message, :user_id, :sent_id)
+  end
 end

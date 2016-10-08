67854ec060793599bde7ff900885d035393b7353
Karivool <evelynlee34@gmail.com>
2016-10-08 18:45:56 -0400

    Create routes for messages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resource :response, only: [:show, :index]
     resources :responses, only: [:create, :show, :index]
     resources :infos, only: [:create, :show, :update, :index]
+    resources :messages, only: [:create, :show, :index]
   end
 
   root to: "static_pages#root"

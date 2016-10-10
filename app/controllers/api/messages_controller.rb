786b4f88063c9c97a83f6b10393f9af120db78cc
Karivool <evelynlee34@gmail.com>
2016-10-09 23:25:51 -0400

    Update messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,11 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    debugger
+    @messageinfo = Message.find(params[:user_id])
+    render "api/messages/index"
   end
 
   def create
+    debugger
+    @messageinfo = Message.new(message.params)
+
+    if @messageinfo.save
+      render "api/messages/index"
+    else
+      render json: @question.errors.full_messages, status: 422
+    end
   end
 
   def show
+    debugger
+    @messageinfo = Message.find(params[:messages][:user_id])
   end
 
   private
